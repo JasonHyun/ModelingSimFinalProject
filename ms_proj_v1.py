552934c6944a4e0fba60c1a51b66011debf91cff
NSDonk <nathanodonkor@gmail.com>
2025-11-29 16:47:44 -0500

    should fix the discrepancies in ODE definitions

--- ms_proj_v1.py
+++ ms_proj_v1.py
@@ -60,7 +60,7 @@ EEP -> Endemic Equilibrium Point
 
 # Deterministic ODEs
 # also may be worth investigating the stochastic case 
-def disease_model(tot_pop, t, M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6): 
+def disease_model(tot_pop, t, M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6, m7): 
     S, V, E, EV, I, IV, Q, QV, H, HV, R, RV = tot_pop
     N = S + V + E + EV + I + IV + Q + QV + H + HV + R + RV # initial total human population
     
@@ -69,32 +69,32 @@ def disease_model(tot_pop, t, M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n,
     l = B * (I + (v1 * IV) + (n * H) + (v2 * HV)) / N
 
     return [ 
-        (1 - p) * N  +  (w * V) - (l * S) - (z + u) * S, # dS/dt
-        (p * M)  +  (z * S)  -  (1 - e) * l * V  -  (w + u) * V, # dV/dt
-        (l * S)  -  (k + o0 + u) * E, # dE/dt
-        (1 - e) * l * V  -  (m1 * k + o1 + u) * EV, # dEV/dt
-        (k * E)  -  (y1 + x + u + d1) * I, # dI/dt
-        (m1 * k) * EV - (m2 * y1  +  m3 * x  +  u  +  m4 * d1) * IV, # dIV/dt
-        (o0 * E)  -  (a  +  u) * Q, # dQ/dt
-        (o1 * EV) - (m5 * a + u) * QV, # dQV/dt
-        (a * Q ) + (x * I) - (y2 + u + d2) * H, # dH/dt
-        (m5 * a * QV) + (m3 * x * IV) - (m6 * y2 + u) * HV, # dHV/dt
+        ((1 - p) * M)  +  (w * V) - (l * S) - ((z + u) * S), # dS/dt
+        (p * M)  +  (z * S)  -  ((1 - e) * l * V)  -  ((w + u) * V), # dV/dt
+        (l * S)  -  ((k + o0 + u) * E), # dE/dt
+        ((1 - e) * l * V)  -  (((m1 * k) + o1 + u) * EV), # dEV/dt
+        (k * E)  -  ((y1 + x + u + d1) * I), # dI/dt
+        (m1 * k * EV) - (((m2 * y1)  + (m3 * x)  +  u  +  (m4 * d1)) * IV), # dIV/dt
+        (o0 * E)  -  ((a  +  u) * Q), # dQ/dt
+        (o1 * EV) - (((m5 * a) + u) * QV), # dQV/dt
+        (a * Q ) + (x * I) - ((y2 + u + d2) * H), # dH/dt
+        (m5 * a * QV) + (m3 * x * IV) - ((m6 * y2 + u + m7 * d2) * HV), # dHV/dt
         (y1 * I) + (y2 * H) - (u * R), #dR/dt
-        (m2 * y1) * IV + (m6 * y2 * HV) - (u * RV) # dRV/dt
+        (m2 * y1 * IV) + (m6 * y2 * HV) - (u * RV) # dRV/dt
     ]
 
 
 def main(): 
-    
+    #''' 
     # Parameter Value Initialization: SARS Model 
     M = 136
-    B = 0.1 #[0.1, 0.2] # variable
+    B = 0.15 #[0.1, 0.2] # variable
     p = 0.25 #[0.25, 0.4] # variable
     u = 0.0000351
     y1 = 0.03521
     y2 = 0.042553
-    d1 = 0.04227
-    d2 = 0.027855
+    d1 = 0 #0.04227
+    d2 = 0 #0.027855
     k =  0.156986
     a = 0.156986
     x = 0.20619
@@ -113,24 +113,55 @@ def main():
     m5 = 0.5
     m6 = 1.4
     m7 = 0.7
-    
+    #'''
+    '''
+        # Parameter Value Initialization: SARS Model 
+    M = 136
+    B = 1.4 #[0.1, 0.2] # variable
+    p = 0.25 #[0.25, 0.4] # variable
+    u = 0.001
+    y1 = 0.03521
+    y2 = 0.042553
+    d1 = 0 #0.04227
+    d2 = 0 #0.027855
+    k =  0.00016
+    a = 0.156986
+    x = 0.20619
+    o0 = 0.1
+    o1 = 0.09
+    w = 0.0001
+    n = 0.8
+    v1 = 0.9
+    v2 = 0.8
+    e  = 0.5 #[0, 1] # variable
+    z  = 0.06
+    m1 = 0.7
+    m2 = 1
+    m3 = 0.7
+    m4 = 0.9
+    m5 = 0.9
+    m6 = 1.4
+    m7 = 0.7
+    '''
     # parameters
-    args = (M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6)
+    args = (M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6, m7)
 
     # time scale
     t = np.linspace(0, 100, 1000)
 
-    # initial conditions
-    inits = np.random.uniform(1000, 2000, size=12)
+    for i in range(10):
+        # initial conditions
+        inits = np.random.uniform(100, 1000, size=12)
 
-    results = scipy.integrate.odeint(disease_model, inits, t, args=args)
+        results = scipy.integrate.odeint(disease_model, inits, t, args=args)
 
-    # order of returned  compartments -> S, V, E, EV, I, IV, Q, QV, H, HV, R, RV
-    infected_individuals = results[:,0]
+        # order of returned  compartments -> S, V, E, EV, I, IV, Q, QV, H, HV, R, RV
+        infected_individuals = results[:,0]
 
-    plt.plot(t, infected_individuals)
-    plt.xlabel("Time")
-    plt.ylabel("Infected Individuals")
+        plt.plot(t, infected_individuals)
+
+    plt.xlabel("Time (days)")
+    plt.ylabel("Total number of infected individuals")
     plt.title("ODE Solution")
     plt.tight_layout()
     # if library just show
