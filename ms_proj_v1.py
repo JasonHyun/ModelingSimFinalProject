1cc95ddc0dd5af629708a3b1ba213142610a5b96
NSDonk <nathanodonkor@gmail.com>
2025-11-29 16:22:41 -0500

    first pass at plotting infected individuals

--- ms_proj_v1.py
+++ ms_proj_v1.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.integrate
+import matplotlib.pyplot as plt
 
 # ok so once able to replicate their results
 # we can try refitting the model (initially performed for SARS) to different diseases (measles, influenza, COVID-19)
@@ -51,35 +53,6 @@ EEP -> Endemic Equilibrium Point
 
 """
 
-# Parameter Value Initialization: SARS Model 
-M = 136
-B = [0.1, 0.2] # variable
-p = [0.25, 0.4] # variable
-u = 0.0000351
-y1 = 0.03521
-y2 = 0.042553
-d1 = 0.04227
-d2 = 0.027855
-k =  0.156986
-a = 0.156986
-x = 0.20619
-o0 = 0.1
-o1 = 0.06
-w = 0.0666
-n = 0.8
-v1 = 0.9
-v2 = 0.8
-e  = [0, 1] # variable
-z  = 0.7
-m1 = 0.6
-m2 = 1.4
-m3 = 0.7
-m4 = 0.6
-m5 = 0.5
-m6 = 1.4
-m7 = 0.7
-
-args = (N, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6)
 # This model extends the SEIQHR model defined in [], by introducing 6 new comparments
 # for vaccinated invidiuals (V, E_v, Q_v, I_v, H_v, R_v). Its purpose is for
 # qualitative analysis and determining the affect of an imperfect vaccine on equilibrium dynamics
@@ -87,13 +60,13 @@ args = (N, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2,
 
 # Deterministic ODEs
 # also may be worth investigating the stochastic case 
-def disease_model(tot_pop, t, N, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6): 
+def disease_model(tot_pop, t, M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6): 
     S, V, E, EV, I, IV, Q, QV, H, HV, R, RV = tot_pop
-    N = sum(S, V, E, EV, I, IV, Q, QV, H, HV, R, RV) # initial total human population
+    N = S + V + E + EV + I + IV + Q + QV + H + HV + R + RV # initial total human population
     
     # λ -> l defines the rate at which unvaccinated susceptible individuals may acquire infection, 
     # following effective contact with infectious individuals
-    l = B(I + (v1 * IV) + (n * H) + (v2 * HV)) / N
+    l = B * (I + (v1 * IV) + (n * H) + (v2 * HV)) / N
 
     return [ 
         (1 - p) * N  +  (w * V) - (l * S) - (z + u) * S, # dS/dt
@@ -104,9 +77,71 @@ def disease_model(tot_pop, t, N, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n,
         (m1 * k) * EV - (m2 * y1  +  m3 * x  +  u  +  m4 * d1) * IV, # dIV/dt
         (o0 * E)  -  (a  +  u) * Q, # dQ/dt
         (o1 * EV) - (m5 * a + u) * QV, # dQV/dt
+        (a * Q ) + (x * I) - (y2 + u + d2) * H, # dH/dt
+        (m5 * a * QV) + (m3 * x * IV) - (m6 * y2 + u) * HV, # dHV/dt
+        (y1 * I) + (y2 * H) - (u * R), #dR/dt
+        (m2 * y1) * IV + (m6 * y2 * HV) - (u * RV) # dRV/dt
     ]
 
 
+def main(): 
+    
+    # Parameter Value Initialization: SARS Model 
+    M = 136
+    B = 0.1 #[0.1, 0.2] # variable
+    p = 0.25 #[0.25, 0.4] # variable
+    u = 0.0000351
+    y1 = 0.03521
+    y2 = 0.042553
+    d1 = 0.04227
+    d2 = 0.027855
+    k =  0.156986
+    a = 0.156986
+    x = 0.20619
+    o0 = 0.1
+    o1 = 0.06
+    w = 0.0666
+    n = 0.8
+    v1 = 0.9
+    v2 = 0.8
+    e  = 0.5 #[0, 1] # variable
+    z  = 0.7
+    m1 = 0.6
+    m2 = 1.4
+    m3 = 0.7
+    m4 = 0.6
+    m5 = 0.5
+    m6 = 1.4
+    m7 = 0.7
+    
+    # parameters
+    args = (M, B, p, u, y1, y2, d1, d2, k, a, x, o0, o1, w, n, v1, v2, e, z, m1, m2, m3, m4, m5, m6)
+
+    # time scale
+    t = np.linspace(0, 100, 1000)
+
+    # initial conditions
+    inits = np.random.uniform(1000, 2000, size=12)
+
+    results = scipy.integrate.odeint(disease_model, inits, t, args=args)
+
+    # order of returned  compartments -> S, V, E, EV, I, IV, Q, QV, H, HV, R, RV
+    infected_individuals = results[:,0]
+
+    plt.plot(t, infected_individuals)
+    plt.xlabel("Time")
+    plt.ylabel("Infected Individuals")
+    plt.title("ODE Solution")
+    plt.tight_layout()
+    # if library just show
+    # plt.show()
+    # save out
+    plt.savefig("infected_individuals_fig1.png", dpi=300)
+
+    
+
+if __name__ == "__main__":
+    main()
 # the parameters needed to numerically demonstrate backwards bifurcation are
 # unrealistic, though the authors do assert that since the phenomena only
 # exists when the effects of vaccines are taken into consideration, which thus
